397bb7bf92804bd74f916935897821afd1009b85
CliconlineStudios <92470785+Cliconline@users.noreply.github.com>
2021-10-20 22:27:09 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,26 +1,66 @@
-function Mostrar() {
-	document.getElementById("informacion").style.display = "block";	
+
+function inicio() {
+    document.getElementById("informacion").style.display = "block";	
     document.getElementById("inicio").style.display = "none";
     document.getElementById("portafolio").style.display = "none";
+    document.getElementById("back").style.display = "none";
+    document.getElementById("costos").style.display = "none";
+    contador=0;
 }
-function Ocultar() {
-	document.getElementById("informacion").style.display = "none";
-    document.getElementById("inicio").style.display = "flex";
+
+
+
+
+var btn = document.getElementById("inicio"),
+    informacion = document.getElementById("infomacion"),
+    contador=0;
+
+
+function servicios() {
+
+if (contador==0) {
+    document.getElementById("informacion").style.display = "none";
+    document.getElementById("inicio").style.display = "block";
     document.getElementById("portafolio").style.display = "block";
+    document.getElementById("back").style.display = "block";
+    contador=1;
+}
+else{
 
 }
 
-function categorias() {
-    document.getElementById("portafolio").style.display = "none";
+
+}
+
+function back() {
+
+    if (contador==1) {
+        document.getElementById("informacion").style.display = "block";
+        document.getElementById("inicio").style.display = "none";
+        document.getElementById("portafolio").style.display = "none";
+        document.getElementById("back").style.display = "none";
+        contador=0;
+    }
+    else{
+        document.getElementById("portafolio").style.display = "block";
+        document.getElementById("costos").style.display = "none";
+        contador=1;
+    }
     
-}
-
-function comofunciona() {
-    document.getElementById("portafolio").style.display = "none";
     
-}
+    }
 
-function planes() {
-    document.getElementById("portafolio").style.display = "none";
-    
-}
+
+    function categorias() {
+        document.getElementById("portafolio").style.display = "none";
+        document.getElementById("costos").style.display = "block";
+        contador=2;
+        
+        
+    }
+
+
+
+
+
+
