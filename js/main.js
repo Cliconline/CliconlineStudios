97b7959b43c9cd198583aa94816e2f215a9af372
CliconlineStudios <92470785+Cliconline@users.noreply.github.com>
2021-11-01 22:46:11 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 
 function inicio() {
     document.getElementById("informacion").style.display = "block";	
-    document.getElementById("inicio").style.display = "none";
     document.getElementById("portafolio").style.display = "none";
     document.getElementById("back").style.display = "none";
     document.getElementById("costos").style.display = "none";
     document.getElementById("comofunciona").style.display = "none";
     document.getElementById("planes").style.display = "none";
+    document.getElementById("side").style.display = "none";
+    conta=0;
     contador=0;
 }
 
@@ -22,9 +23,10 @@ function servicios() {
 
 if (contador==0) {
     document.getElementById("informacion").style.display = "none";
-    document.getElementById("inicio").style.display = "block";
     document.getElementById("portafolio").style.display = "block";
     document.getElementById("back").style.display = "block";
+    document.getElementById("side").style.display = "none";
+    conta=0;
     contador=1;
 }
 else{
@@ -38,10 +40,19 @@ function back() {
 
     if (contador==1) {
         document.getElementById("informacion").style.display = "block";
-        document.getElementById("inicio").style.display = "none";
         document.getElementById("portafolio").style.display = "none";
         document.getElementById("back").style.display = "none";
+        document.getElementById("side").style.display = "none";
+        conta=0;
         contador=0;
+        document.getElementById("back")
+        .addEventListener('click',()=>{
+         window.scrollTo({
+         top: 0,
+         behavior: 'smooth'
+        });
+        });
+
     }
     else{
         document.getElementById("portafolio").style.display = "block";
@@ -49,6 +60,14 @@ function back() {
         document.getElementById("comofunciona").style.display = "none";
         document.getElementById("planes").style.display = "none";
         contador=1;
+        document.getElementById("back")
+        .addEventListener('click',()=>{
+         window.scrollTo({
+         top: 0,
+         behavior: 'smooth'
+        });
+        });
+
     }
     
     
@@ -56,8 +75,15 @@ function back() {
 
 
     function categorias() {
+        document.getElementById("informacion").style.display = "none";	
         document.getElementById("portafolio").style.display = "none";
         document.getElementById("costos").style.display = "block";
+        document.getElementById("back").style.display = "block";
+        document.getElementById("side").style.display = "none";
+        document.getElementById("informacion").style.display = "none";
+        document.getElementById("planes").style.display = "none";
+        document.getElementById("comofunciona").style.display = "none";
+        conta=0;
         contador=2;
         
         
@@ -66,6 +92,11 @@ function back() {
     function comofunciona() {
         document.getElementById("portafolio").style.display = "none";
         document.getElementById("comofunciona").style.display = "block";
+        document.getElementById("side").style.display = "none";
+        document.getElementById("informacion").style.display = "none";
+        document.getElementById("back").style.display = "block";
+        document.getElementById("costos").style.display = "none";
+        conta=0;
         contador=2;
         
         
@@ -74,10 +105,53 @@ function back() {
     function planes() {
         document.getElementById("planes").style.display = "block";
         document.getElementById("portafolio").style.display = "none";
+        document.getElementById("side").style.display = "none";
+        document.getElementById("informacion").style.display = "none";
+        document.getElementById("back").style.display = "block";
+        document.getElementById("comofunciona").style.display = "none";
+        document.getElementById("costos").style.display = "none";
+        conta=0;
         contador=3;
         
         
     }
+
+    var btns = document.getElementById("inicio"),
+    informacion = document.getElementById("infomacion"),
+    conta=0;
+
+    function sidebar()  {
+
+        if (conta==0) {
+            document.getElementById("side").style.display = "block";
+            conta=1;
+        }
+        else{
+            document.getElementById("side").style.display = "none";
+            conta=0;
+        }
+        }
+
+        function cerrarside() {
+            document.getElementById("side").style.display = "none";
+            conta=0;
+            
+        }
+
+
+       
+        document.querySelector('.tarjeta')  
+        .addEventListener('click',()=>{
+         window.scrollTo({
+         top: 0,
+         behavior: 'smooth'
+        });
+        });
+
+      
+
+        
+        
 
 
 
