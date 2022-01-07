e5a005ce745c38c66aacab3584e847420f695f9d
CliconlineStudios <92470785+Cliconline@users.noreply.github.com>
2022-01-06 21:23:11 -0500

    Add files via upload

---
+++ js/negocios.js
@@ -0,0 +1,28 @@
+
+
+
+             //    menu laterl 
+
+    var btns = document.getElementById("inicio"),
+    informacion = document.getElementById("infomacion"),
+    conta=0;
+
+    function sidebar()  {
+
+        if (conta==0) {
+            document.getElementById("side").style.display = "block";
+            conta=1;
+        }
+        else{
+            document.getElementById("side").style.display = "none";
+            conta=0;
+        }
+        }
+
+        function cerrarside() {
+            document.getElementById("side").style.display = "none";
+            conta=0;
+            
+        }     
+  
+
